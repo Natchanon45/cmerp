1c3e57eb5ea0042c2e4d45108a5476f92a9a9662
Kant <javasci@gmail.com>
2023-06-02 17:09:24 +0700

    ถึงขั้นตอนการทำ control แบ่งจ่าย

--- application/views/clients/view.php
+++ application/views/clients/view.php
@@ -41,7 +41,7 @@
             <li><a  role="presentation" href="<?php echo_uri("clients/orders/" . $client_info->id); ?>" data-target="#client-orders"> <?php echo lang('orders'); ?></a></li>
         <?php } ?>
         <?php if ($show_estimate_request_info) { ?>
-            <li><a  role="presentation" href="<?php echo_uri("clients/estimate_requests/" . $client_info->id); ?>" data-target="#client-estimate-requests"> <?php echo lang('estimate_requests'); ?></a></li>
+            <!--<li><a  role="presentation" href="<?php echo_uri("clients/estimate_requests/" . $client_info->id); ?>" data-target="#client-estimate-requests"> <?php echo lang('estimate_requests'); ?></a></li>-->
         <?php } ?>
         <?php if ($show_ticket_info) { ?>
             <li><a  role="presentation" href="<?php echo_uri("clients/tickets/" . $client_info->id); ?>" data-target="#client-tickets"> <?php echo lang('tickets'); ?></a></li>
