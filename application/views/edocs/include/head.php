6890034e90dccd3b311db77529f43f708cb41b1b
Kant <javasci@gmail.com>
2023-06-28 14:09:40 +0700

    fix share view doc

--- application/views/edocs/include/head.php
+++ application/views/edocs/include/head.php
@@ -4,4 +4,4 @@
 <link rel="stylesheet" href="/assets/js/font-awesome/css/font-awesome.min.css">
 <link rel="stylesheet" href="/assets/css/color/<?php echo get_setting("default_theme_color"); ?>.css">
 <link rel="stylesheet" href="/assets/css/font.css">
-<link rel="stylesheet" href="/assets/css/edocs.css">
+<link rel="stylesheet" href="/assets/css/edocs.css?t=<?php echo time(); ?>">
